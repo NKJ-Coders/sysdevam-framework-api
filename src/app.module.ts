d4e22cadbaeac8116dbc1245ea5298076c86b6dd
NKJ-Coders <idrissketchaya@gmail.com>
2021-07-15 23:41:23 +0100

    organize files in current directories

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AccessRightModule } from './access-right/access-right.module';
+import { TemplateModule } from './template/template.module';
+import { TransactionModule } from './transaction/transaction.module';
+import { TransactionTemplateModule } from './transaction-template/transaction-template.module';
+import { UserModule } from './user/user.module';
+import { UserTransactionModule } from './user-transaction/user-transaction.module';
 
 @Module({
-  imports: [],
+  imports: [AccessRightModule, TemplateModule, TransactionModule, TransactionTemplateModule, UserModule, UserTransactionModule],
   controllers: [AppController],
   providers: [AppService],
 })
